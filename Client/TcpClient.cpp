4b975730bd5d404736eeefd3f7a5538e2780f3b8
Vorduk <118179575+Vorduk@users.noreply.github.com>
2026-05-21 09:57:46 +0700

    Update TcpClient.cpp, added lefover bytes saving after request

--- Client/TcpClient.cpp
+++ Client/TcpClient.cpp
@@ -36,16 +36,16 @@ bool TcpClient::connectToServer() {
     m_socket = socket(AF_INET, SOCK_STREAM, 0);
     if (m_socket == INVALID_SOCKET) return false;
 #else
-    m_socket = socket(AF_INET, SOCK_STREAM, 0);
-    if (m_socket < 0) return false;
+    m_socket = socket(AF_INET, SOCK_STREAM, 0); // IpV4; stream socket; TCP.
+    if (m_socket < 0) return false; // No sockets left or incorrect params were passed to function.
 #endif
 
     struct sockaddr_in addr; // Server address structure.
     addr.sin_family = AF_INET; // IPv4.
-    addr.sin_port = htons(m_port); // Port (Host TO Network Short).
-    inet_pton(AF_INET, m_server_address.c_str(), &addr.sin_addr); // Ip to binary.
+    addr.sin_port = htons(m_port); // Port (Host TO Network Short, little endian to big endian).
+    inet_pton(AF_INET, m_server_address.c_str(), &addr.sin_addr); // Ip to bytes.
 
-    // Tcp connection.
+    // Tcp connection. 3-way handshake.
     if (connect(m_socket, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
 #ifdef _WIN32
         closesocket(m_socket);
@@ -81,11 +81,27 @@ std::string TcpClient::sendRequest(const std::string& request_json) {
 #else
         int bytes_readed = recv(m_socket, buf, sizeof(buf) - 1, 0);
 #endif
-        if (bytes_readed <= 0) break; // Server closed connection.
-        response.append(buf, bytes_readed); // Add new bytes to response string
-        if (response.find('\n') != std::string::npos) break; // Response fully transfered, end loop.
+        if (bytes_readed <= 0) {
+            // Disconnected or error.
+            m_is_server_connected = false;
+            m_socket = -1;
+            return R"({"status":"error","message":"Server disconnected"})";
+        }
+        m_leftover_buffer.append(buf, bytes_readed); // Add leftover bytes from previous request.
+
+        if (m_leftover_buffer.find('\n') != std::string::npos) break; // Exit after full message was acquired.
     }
 
-    if (!response.empty() && response.back() == '\n') response.pop_back(); // Delete "\n", doesn't need in Json parsing.
+    // Get first full message (to and with '\n')
+    size_t newline_pos = m_leftover_buffer.find('\n');
+    if (newline_pos == std::string::npos) {
+        return R"({"status":"error","message":"Invalid server response"})";
+    }
+    std::string response = m_leftover_buffer.substr(0, newline_pos);
+
+    // Delete used part from the buffer (including '\n').
+    // Leftover left.
+    m_leftover_buffer.erase(0, newline_pos + 1);
+
     return response;
 }
