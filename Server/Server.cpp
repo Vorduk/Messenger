11b4dd449b0382a9c5eabe1f335032375a27930f
kudro <vorduk100@mail.ru>
2026-05-25 23:26:12 +0700

    Added openssl

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,10 +1,18 @@
 ﻿#include "TestServer.h"
 #include "Logger.h"
 
+#include <iostream>
+
+#include <openssl/ssl.h>
+
 int main() {
     Logger::getInstance().initialize("server.log");
     Logger::getInstance().setMinimalLevel(Logger::Level::Info);
     TestServer server(8080, "server.db");
+
+    std::cout << "OpenSSL version: " << SSLeay_version(SSLEAY_VERSION) << std::endl;
+    std::cout << "OpenSSL version number: " << SSLeay() << std::endl;
+
     server.run();
     return 0;
 }
